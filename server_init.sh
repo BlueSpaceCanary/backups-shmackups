005dbe9135db7d95f7b300e0e62abe866da8a994
Philip Davis <phs.philip@gmail.com>
2016-03-09 12:23:20 -0800

    Add help to server_init

--- server_init.sh
+++ server_init.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# USAGE: ./server_init.sh
+
 local S3QLFS=""
 local STAGING=""
 local ENC_STAGING=""
+
+help() {
+    echo "Performs initial setup for a machine to act as a backup-schmackup server.
+--s3ql foo will create a S3QL filesystem called foo.
+--enc_staging instructs us to create an EncFS of the staging directory.
+--staging foo specifies that foo is the directory into which data to be backed up is staged
+--help and -h print this message."
+}
 
 while true; do
     case "$1" in
@@ -39,7 +49,7 @@ main() {
        mkfs.s3ql "$S3QLFS"
     fi
 
-    # If we're using an ENCFS, mount it.
+    # If we're using an ENCFS, mount it. Might also create it.
     if [ ! -z "$ENC_STAGING" ]; then
         encfs --reverse "$STAGING" "$ENC_STAGING" 
     fi
