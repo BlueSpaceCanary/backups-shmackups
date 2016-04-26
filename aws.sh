9ca1a266dc77d1259736602ae89b1e1be30b4ef9
Philip Davis <phs.philip@gmail.com>
2016-04-26 11:06:58 -0700

    Add AWS script

---
+++ aws.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+AWS_TARGET=""
+AWS_SOURCE=""
+DRY_RUN=false
+while true; do
+    case $1 in
+        -h|--help)
+            help
+            exit 0
+            ;;
+        --target)
+            shift
+            AWS_TARGET="$1"
+            ;;
+        --source)
+            shift
+            AWS_SOURCE="$1"
+            ;;
+        --dry-run)
+            shift
+            DRY_RUN=true
+        --)
+            shift && break # -- ends options
+            ;;
+        *-) echo "$0: Unrecognized option $1" >&2
+            exit 1
+            ;;
+        *) break
+           ;;
+    esac
+done
+
+main() {
+    local DATE=$(date +%Y-%m-%d)
+    local DAILY="$TARGET/daily/$DATE"
+    echo "Uploading daily to $TARGET/daily/$DATE"
+    if [ "$bool" != true ]; then
+        aws s3 "$SOURCE" "$TARGET/daily/$DATE"
+    fi
+}
+
+main()
